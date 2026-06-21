37dae21551c24c64962c081b1aa2483d2d5309b5
harshithacharya632-source <harshithacharya632@gmail.com>
2026-06-21 15:00:26 +0530

    Update info.py

--- info.py
+++ info.py
@@ -47,7 +47,7 @@ SHORTLINK_URL = environ.get('SHORTLINK_URL', 'techvjlink.site')  # Shortener sit
 SHORTLINK_API = environ.get('SHORTLINK_API', 'd73e70a35dc3877fa14afbf51fa8ec312c94780c')  # API key for shortlink
 
 # 💾 MongoDB Connection Information
-DB_URL = environ.get('DATABASE_URI', "mongodb+srv://harshithacharya633:2g91drYh3nV4GDhW@cluster0.3yqvc4p.mongodb.net/?retryWrites=true&w=majority&appName=Cluster0")  # MongoDB connection URI
+#DB_URL = environ.get('DATABASE_URI', "mongodb+srv://harshithacharya633:2g91drYh3nV4GDhW@cluster0.3yqvc4p.mongodb.net/?retryWrites=true&w=majority&appName=Cluster0")  # MongoDB connection URI
 DB_NAME = environ.get('DATABASE_NAME', "cluster0")  # MongoDB database name
 
 # 📸 all Media (Images)
